983d22cac34c6fd55c103e17798278d6a0b2007c
EzeLunaWatkins <eilw2000@hotmail.com>
2016-06-13 16:44:37 -0300

    Initial commit

---
+++ app/src/main/java/ui/unq/ezelunawatkins/acahaygatoencerrado/InventarioActivity.java
@@ -0,0 +1,16 @@
+package ui.unq.ezelunawatkins.acahaygatoencerrado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventarioActivity extends AppCompatActivity {
+
+    private Inventario inventario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+        this.inventario = new Inventario();
+    }
+}
