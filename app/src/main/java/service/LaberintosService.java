384e14482fcfdac82f6e9a09c4876e902cb4be0a
EzeLunaWatkins <eilw2000@hotmail.com>
2016-06-26 16:35:39 -0300

    Creación de Servicios para uso del inventario en LaberintosService

--- app/src/main/java/service/LaberintosService.java
+++ app/src/main/java/service/LaberintosService.java
@@ -2,6 +2,8 @@ package service;
 
 import java.util.List;
 
+import model.MinInventario;
+import model.MinItem;
 import model.MinLaberinto;
 import retrofit.Callback;
 import retrofit.http.GET;
@@ -13,4 +15,10 @@ public interface LaberintosService {
 
     @GET("/laberintos/{LaberintoId}")
     void getLaberinto(@retrofit.http.Path("LaberintoId") String id, Callback<MinLaberinto> callback);
+
+    @GET("/inventario")
+    void getInventario(Callback<List<MinInventario>> callback);
+
+    @GET("/tirar/{ItemId}")
+    void tirarItem(@retrofit.http.Path("ItemId") String id, Callback<MinItem> callback);
 }
