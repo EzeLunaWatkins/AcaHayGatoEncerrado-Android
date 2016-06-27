fb6ba4676cddf1d5ac810564c0d7ea2c7c23f38d
EzeLunaWatkins <eilw2000@hotmail.com>
2016-06-27 01:11:17 -0300

    Actualización IPs

--- app/src/main/java/service/LaberintosServiceFactory.java
+++ app/src/main/java/service/LaberintosServiceFactory.java
@@ -4,7 +4,7 @@ import retrofit.RestAdapter;
 
 public class LaberintosServiceFactory {
 
-    static public String SERVER_IP = "181.44.153.29";
+    static public String SERVER_IP = "0.0.0.0";
     static public String API_URL = "http://" + SERVER_IP + ":9777";
 
     static public LaberintosService createLaberintosService(){
